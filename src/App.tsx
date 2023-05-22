aeab7c674eef92689c52879f862ef50b1c9e49ed
adigogoi <aditi@techvariable.com>
2023-05-22 12:07:59 +0530

    whatsApp_clone

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+
+import './App.css'
+
+export default function App() {
+  return (
+    <h1 className="p-1 bg-mycolor text-green-500  text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  )
+}
